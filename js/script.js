5c95a8b55beb54ec838c3d97fad1c8493520e451
Karla Avila <karla.e.avila@gmail.com>
2020-08-22 13:03:24 -0700

    Added some titles to my portfolio.

--- js/script.js
+++ js/script.js
@@ -7,5 +7,12 @@ $('#slides').superslides({
 	pagination: false
 });
 
+var typed = new Typed(".typed", {
+	strings: ["Web Developer.", "Frontend Engineer.", "HCI Specialist."],
+	typeSpeed: 70,
+	loop: true,
+	startDelay: 1000,
+	showCursor: false
+});
 
 });
